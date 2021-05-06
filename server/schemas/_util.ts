52e04d3e046709502e30d616af173f3b088f0425
Debashish Patra <suvam0451@protonmail.com>
2021-05-06 16:30:07 +0530

    fix: fix containner name where mongoose connects to mongo container

--- server/schemas/_util.ts
+++ server/schemas/_util.ts
@@ -4,7 +4,7 @@ import * as dotenv from "dotenv";
 dotenv.config()
 
 
-const uri = process.env.CONTAINERIZED == "true" ? "mongodb://mongo:27017/local" : "mongodb://127.0.0.1:27017/local";
+const uri = process.env.CONTAINERIZED == "true" ? "mongodb://automemory_mongo:27017/local" : "mongodb://127.0.0.1:27017/local";
 
 export function connectMongoose() {
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
