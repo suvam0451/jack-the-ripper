1b01fd8ddee7e58ab20ed6f8a37ae8dd6df47046
Debashish Patra <suvam0451@protonmail.com>
2021-05-06 15:52:04 +0530

    fix(podman): point mongoose connection to intranet of compose

--- server/schemas/_util.ts
+++ server/schemas/_util.ts
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb://127.0.0.1:27017/local";
+import * as dotenv from "dotenv";
+dotenv.config()
+
+
+const uri = process.env.CONTAINERIZED == "true" ? "mongodb://mongo:27017/local" : "mongodb://127.0.0.1:27017/local";
 
 export function connectMongoose() {
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
